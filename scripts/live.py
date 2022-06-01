369ae0874f021e8778ec01ab6ded8511561e8c33
Naza <uzoukwuc@tcd.ie>
2022-06-01 03:02:06 +0100

    Fixed bugs in refactor

--- scripts/live.py
+++ scripts/live.py
@@ -27,16 +27,20 @@ def mask_to_color(prediction):
     height = prediction.shape[1]
     return colors[prediction.cpu().numpy()]
 
-def predict(image, resize=True, color=True):
-    '''Takes a numpy HWC, BGR image, returns a numpy HWC, BGR image with the user friendly masks'''
-    if resize:
-        new_height = int(512 * image.shape[1] / image.shape[0])
-        image = cv.resize(image, (new_height - new_height % 64, 512), interpolation=cv.INTER_NEAREST)
+def scale(image, output_width):
+    new_height = int(output_width * image.shape[1] / image.shape[0])
+    return cv.resize(image, (new_height - new_height % 64, output_width), interpolation=cv.INTER_NEAREST)
+
+def predict(image, resize=True):
+    '''Takes an OpenCV BGR image and returns class probabilities with the shape HxWx5'''
     rgb_image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
     normal = F.Normalize([.485, .456, .406], [.229, .224, .225])
     tensor = normal(torch.from_numpy(rgb_image.transpose(2, 0, 1)).to(device).float())
-    pred = torch.nn.Softmax(0)(model(tensor[None, ...])[0][0])
-    return image, cv.cvtColor(mask_to_color(pred.argmax(0)), cv.COLOR_RGB2BGR) if color else pred
+    return torch.nn.Softmax(0)(model(tensor[None, ...])[0][0])
+
+def colorise(pred):
+    '''Takes predictions returned from `predict` and returns a BGR image with the shape HxWx3'''
+    return cv.cvtColor(mask_to_color(pred.argmax(0)), cv.COLOR_RGB2BGR)
 
 def merge(image, mask, image_vs_mask=0.5):
     image_and_mask = np.uint8(image * image_vs_mask + mask * (1 - image_vs_mask))
@@ -59,7 +63,18 @@ if __name__ == "__main__":
                         help='the path where the mask will be saved')
     parser.add_argument('--camera', type=int, default=0,
                         help='which camera to use')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--image-intensity', type=int, default=0.5,
+                        help='multiplier that will be applied to the image when it\'s added to the mask')
+    group.add_argument('--mask-intensity', type=int, default=0.5,
+                        help='multiplier that will be applied to the mask when it\'ts added to the image')
     args = parser.parse_args()
+
+    if args.mask_intensity != 0.5:
+        intensity = 1 - args.mask_intensity
+    else:
+        intensity = args.image_intensity
+    assert 0 <= intensity and intensity <= 1, "Intensity must be in [0, 1]"
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = get_segmentation_model(backbone=args.backbone, pretrained_base=False, model=args.model, dataset=args.dataset, norm_layer=nn.BatchNorm2d).to(device)
@@ -71,10 +86,11 @@ if __name__ == "__main__":
         cam = cv.VideoCapture(args.camera)
         while True:
             result, image = cam.read()
-            show(merge(*predict(image)))
+            image = scale(image, 512)
+            show(merge(image, colorise(predict(image)), image_vs_mask=intensity), interval=1)
     else:
         original_image = cv.imread(args.input)
-        mask = merge(*predict(original_image))
+        mask = merge(original_image, colorise(predict(original_image)))
         if args.output:
             cv.imwrite(args.output, mask)
         else:
