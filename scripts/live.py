de5bdf4d114c7c00889a4866299450e63e8e4de8
Naza <uzoukwuc@tcd.ie>
2022-06-01 02:40:24 +0100

    Added option for colorising vs. returning raw predictions.

--- scripts/live.py
+++ scripts/live.py
@@ -14,22 +14,7 @@ sys.path.append(root_path)
 
 from fta.models.model_zoo import get_segmentation_model
 
-parser = argparse.ArgumentParser(description='Live preview')
-parser.add_argument('--model', default='cgnet')
-parser.add_argument('--dataset', default='ughent')
-parser.add_argument('--backbone', default='resnet50')
-parser.add_argument('--weights', required=True)
-parser.add_argument('--input',
-                    help='the path to the input image')
-parser.add_argument('--output',
-                    help='the path where the mask will be saved')
-parser.add_argument('--camera', type=int, default=0,
-                   help='which camera to use')
-args = parser.parse_args()
-
-
 def mask_to_color(prediction):
-    # This really could be an array
     colors = np.array([
         [0, 0, 0], # Background
         [0, 0, 255], # Blue
@@ -42,13 +27,7 @@ def mask_to_color(prediction):
     height = prediction.shape[1]
     return colors[prediction.cpu().numpy()]
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = get_segmentation_model(backbone=args.backbone, pretrained_base=False, model=args.model, dataset=args.dataset, norm_layer=nn.BatchNorm2d).to(device)
-model.load_state_dict(torch.load(args.weights, map_location=device))
-
-model.train()
-
-def predict(image, resize=True):
+def predict(image, resize=True, color=True):
     '''Takes a numpy HWC, BGR image, returns a numpy HWC, BGR image with the user friendly masks'''
     if resize:
         new_height = int(512 * image.shape[1] / image.shape[0])
@@ -57,8 +36,7 @@ def predict(image, resize=True):
     normal = F.Normalize([.485, .456, .406], [.229, .224, .225])
     tensor = normal(torch.from_numpy(rgb_image.transpose(2, 0, 1)).to(device).float())
     pred = torch.nn.Softmax(0)(model(tensor[None, ...])[0][0])
-    #pred[0, pred[0, ...] > 0.2] = 1
-    return image, cv.cvtColor(mask_to_color(pred.argmax(0)), cv.COLOR_RGB2BGR)
+    return image, cv.cvtColor(mask_to_color(pred.argmax(0)), cv.COLOR_RGB2BGR) if color else pred
 
 def merge(image, mask, image_vs_mask=0.5):
     image_and_mask = np.uint8(image * image_vs_mask + mask * (1 - image_vs_mask))
@@ -67,19 +45,37 @@ def merge(image, mask, image_vs_mask=0.5):
 def show(image, interval=10000):
     cv.imshow("Mask and Image", image)
     cv.moveWindow("Mask and Image", 30, 30)
-    #cv.imshow("Mask only", mask)
-    #cv.imwrite(f"test/{random.randint(0, 1000000)}.png", result)
     cv.waitKey(interval)
 
-if args.input is None:
-    cam = cv.VideoCapture(args.camera)
-    while True:
-        result, image = cam.read()
-        show(merge(*predict(image)))
-else:
-    original_image = cv.imread(args.input)
-    mask = merge(*predict(original_image))
-    if args.output:
-        cv.imwrite(args.output, mask)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Live preview')
+    parser.add_argument('--model', default='cgnet')
+    parser.add_argument('--dataset', default='ughent') # The dataset just gives the number of classes...
+    parser.add_argument('--backbone', default='resnet50')
+    parser.add_argument('--weights', required=True)
+    parser.add_argument('--input',
+                        help='the path to the input image')
+    parser.add_argument('--output',
+                        help='the path where the mask will be saved')
+    parser.add_argument('--camera', type=int, default=0,
+                        help='which camera to use')
+    args = parser.parse_args()
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = get_segmentation_model(backbone=args.backbone, pretrained_base=False, model=args.model, dataset=args.dataset, norm_layer=nn.BatchNorm2d).to(device)
+    model.load_state_dict(torch.load(args.weights, map_location=device))
+    
+    model.train()
+        
+    if args.input is None:
+        cam = cv.VideoCapture(args.camera)
+        while True:
+            result, image = cam.read()
+            show(merge(*predict(image)))
     else:
-        show(mask)
+        original_image = cv.imread(args.input)
+        mask = merge(*predict(original_image))
+        if args.output:
+            cv.imwrite(args.output, mask)
+        else:
+            show(mask)
