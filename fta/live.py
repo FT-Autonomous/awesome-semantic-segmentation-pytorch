33e1bb24829dcf56eebdc6d979e036b9795efec0
Naza <uzoukwuc@tcd.ie>
2022-06-04 02:48:22 +0100

    General cleanup, type changes

--- fta/live.py
+++ fta/live.py
@@ -6,7 +6,6 @@ import os
 import sys
 import argparse
 import torch
-from torch import nn
 
 from .models.model_zoo import get_segmentation_model
 
@@ -18,25 +17,26 @@ def mask_to_color(prediction):
         [100, 50, 0], # Large Orange
         [120, 30, 0]  # Orange 
     ], dtype=np.uint8)
-    
-    width = prediction.shape[0]
-    height = prediction.shape[1]
-    return colors[prediction.cpu().numpy()]
+
+    return colors[prediction]
 
 def scale(image, output_width):
     new_height = int(output_width * image.shape[1] / image.shape[0])
     return cv.resize(image, (new_height - new_height % 64, output_width), interpolation=cv.INTER_NEAREST)
 
-def predict(image, model, device, resize=True):
+def predict_raw(image, model, device):
     '''Takes an OpenCV BGR image and returns class probabilities with the shape HxWx5'''
     rgb_image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
     normal = F.Normalize([.485, .456, .406], [.229, .224, .225])
     tensor = normal(torch.from_numpy(rgb_image.transpose(2, 0, 1)).to(device).float())
     return torch.nn.Softmax(0)(model(tensor[None, ...])[0][0])
 
+def predict(*args, **kwargs):
+    return predict_raw(*args, **kwargs).argmax(0).cpu().numpy()
+
 def colorise(pred):
     '''Takes predictions returned from `predict` and returns a BGR image with the shape HxWx3'''
-    return cv.cvtColor(mask_to_color(pred.argmax(0)), cv.COLOR_RGB2BGR)
+    return cv.cvtColor(mask_to_color(pred), cv.COLOR_RGB2BGR)
 
 def merge(image, mask, image_vs_mask=0.5):
     image_and_mask = np.uint8(image * image_vs_mask + mask * (1 - image_vs_mask))
@@ -83,10 +83,10 @@ if __name__ == "__main__":
         while True:
             result, image = cam.read()
             image = scale(image, 512)
-            show(merge(image, colorise(predict(image, device)), image_vs_mask=intensity), interval=1)
+            show(merge(image, colorise(predict(image, model, device)), image_vs_mask=intensity), interval=1)
     else:
         original_image = cv.imread(args.input)
-        mask = merge(original_image, colorise(predict(original_image)))
+        mask = merge(original_image, colorise(predict(original_image, model, device)))
         if args.output:
             cv.imwrite(args.output, mask)
         else:
